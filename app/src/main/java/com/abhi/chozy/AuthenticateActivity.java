6ffec3dd278f66ef3f100d0649d382089a996732
abhishek dananjaya <abhishekdananjaya1@yahoo.com>
2021-09-27 09:23:02 +0530

    Initial commit

---
+++ app/src/main/java/com/abhi/chozy/AuthenticateActivity.java
@@ -0,0 +1,14 @@
+package com.abhi.chozy;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AuthenticateActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_authenticate);
+    }
+}
