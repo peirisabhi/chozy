c30e4b6773b5953287982e15baf7b085eca101d4
abhishek dananjaya <abhishekdananjaya1@yahoo.com>
2021-10-10 19:12:56 +0530

    image slider added

---
+++ app/src/main/java/com/abhi/chozy/model/ImageSliderItem.java
@@ -0,0 +1,17 @@
+package com.abhi.chozy.model;
+
+public class ImageSliderItem {
+    private int image;
+
+    public ImageSliderItem(int image) {
+        this.image = image;
+    }
+
+    public int getImage() {
+        return image;
+    }
+
+    public void setImage(int image) {
+        this.image = image;
+    }
+}
--- app/src/main/java/com/abhi/chozy/ui/home/HomeFragment.java
+++ app/src/main/java/com/abhi/chozy/ui/home/HomeFragment.java
@@ -1,6 +1,7 @@
 package com.abhi.chozy.ui.home;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,13 +12,30 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager2.widget.CompositePageTransformer;
+import androidx.viewpager2.widget.MarginPageTransformer;
+import androidx.viewpager2.widget.ViewPager2;
 
+import com.abhi.chozy.R;
+import com.abhi.chozy.adapter.ImageSliderAdapter;
 import com.abhi.chozy.databinding.FragmentHomeBinding;
+import com.abhi.chozy.model.ImageSliderItem;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeFragment extends Fragment {
 
     private HomeViewModel homeViewModel;
     private FragmentHomeBinding binding;
+
+    private Handler sliderHandler = new Handler();
+    private static final int SLIDER_TIME_OUT = 3000;
+
+    ViewPager2 viewPager2;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
@@ -36,6 +54,72 @@ public class HomeFragment extends Fragment {
 //        });
         return root;
     }
+
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        viewPager2 = binding.viewPagerImageSlider;
+
+        loadImageSlider();
+    }
+
+
+    private void loadImageSlider() {
+        List<ImageSliderItem> imageSliderItems = new ArrayList<>();
+        imageSliderItems.add(new ImageSliderItem(R.drawable.offer));
+        imageSliderItems.add(new ImageSliderItem(R.drawable.offer));
+        imageSliderItems.add(new ImageSliderItem(R.drawable.offer));
+
+        viewPager2.setAdapter(new ImageSliderAdapter(imageSliderItems, viewPager2, requireActivity()));
+
+        viewPager2.setClipToPadding(false);
+        viewPager2.setClipChildren(false);
+        viewPager2.setOffscreenPageLimit(3);
+        viewPager2.getChildAt(0).setOverScrollMode(RecyclerView.OVER_SCROLL_NEVER);
+
+        CompositePageTransformer compositePageTransformer = new CompositePageTransformer();
+        compositePageTransformer.addTransformer(new MarginPageTransformer(25));
+        compositePageTransformer.addTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float r = 1 - Math.abs(position);
+                page.setScaleY(0.85f + r * 0.15f);
+            }
+        });
+
+        viewPager2.setPageTransformer(compositePageTransformer);
+
+        viewPager2.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                sliderHandler.removeCallbacks(sliderRunnable);
+                sliderHandler.postDelayed(sliderRunnable, SLIDER_TIME_OUT);
+            }
+        });
+    }
+
+
+    private Runnable sliderRunnable = new Runnable() {
+        @Override
+        public void run() {
+            viewPager2.setCurrentItem(viewPager2.getCurrentItem() + 1);
+        }
+    };
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        sliderHandler.removeCallbacks(sliderRunnable);
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        sliderHandler.postDelayed(sliderRunnable, SLIDER_TIME_OUT);
+    }
+
 
     @Override
     public void onDestroyView() {
